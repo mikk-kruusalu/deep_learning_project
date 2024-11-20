3d1b646cbb9c8735ca2e4ebca885d6bae450251a
Mikk Kruusalu <kruusalu.mikk@gmail.com>
2024-11-20 20:57:13 +0200

    Fine tuned training and evaluation scripts. Now only one script for each

---
+++ cnn/evaluate.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import torch
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from train import TrainingMetrics, models
+import argparse
+from pathlib import Path
+from dataset import load_data
+from torch.utils.data import DataLoader
+
+
+def plot_learning_curves(metrics: TrainingMetrics):
+    fig, ax = plt.subplots(1, 2)
+    ax[0].plot(metrics.test_losses, label="test")
+    ax[0].plot(metrics.train_losses, label="train")
+    ax[0].set_title("Losses")
+    ax[0].legend()
+
+    ax[1].plot(metrics.test_f1s, label="test")
+    ax[1].plot(metrics.train_f1s, label="train")
+    ax[1].set_title("F1 score")
+    ax[1].legend()
+
+    return fig
+
+
+def plot_confusion_matrix(loader, model, classes, device="cpu", ax=None, **fig_kw):
+    # model.eval()
+    test_pred = []
+    test_labels = []
+    for img, label in loader:
+        img = img.to(device)
+        test_pred.append(model(img).detach().cpu())
+        test_labels.append(label)
+
+    test_pred = torch.concatenate(test_pred)
+    test_labels = torch.concatenate(test_labels)
+    test_pred = torch.argmax(test_pred, dim=-1)
+
+    cm_display = ConfusionMatrixDisplay(confusion_matrix(test_labels, test_pred), display_labels=classes)
+
+    if ax is None:
+        fig, ax = plt.subplots(constrained_layout=True, **fig_kw)
+    cm_display.plot(xticks_rotation=45, ax=ax)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True)
+    parser.add_argument("-m", "--model", required=True)
+    parser.add_argument("-d", "--device", default="cpu")
+    parser.add_argument("-b", "--batch_size", default=64)
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    file = Path(args.file)
+
+    d = torch.load(file, weights_only=False)
+    model = models[args.model]()
+    model.load_state_dict(d["model"])
+    print(model)
+    metrics = TrainingMetrics(**d["metrics"])
+    print(metrics)
+
+    plot_learning_curves(metrics)
+    plt.savefig(file.parent / f"{file.stem}_learning_curves.png")
+
+    _, test_data = load_data()
+    test_loader = DataLoader(test_data, batch_size=args.batch_size)
+    plot_confusion_matrix(test_loader, model, test_data.classes, args.device)
+    plt.savefig(file.parent / f"{file.stem}_confusion_matrix.png")
--- cnn/train.py
+++ cnn/train.py
@@ -1,43 +1,132 @@
 import argparse
-import os
-
+from pathlib import Path
+from dataclasses import dataclass, field, asdict
 import torch
 import yaml
 from dataset import load_data
-from simplecnn import SimpleCNN
 from torch.utils.data import DataLoader
-from training import train
+from simplecnn import SimpleCNN
 from unet_transfer import UnetTransfer
+from sklearn.metrics import f1_score
 
-# parse args
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--config", required=True)
-parser.add_argument("-o", "--output-dir", default="cnn/checkpoints")
-parser.add_argument("-d", "--device", default="cpu")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--simplecnn", action="store_true", help="Use Simple CNN model")
-group.add_argument("--unet", action="store_true", help="Use UNet transfer learning model")
 
-args = parser.parse_args()
+models = {
+    "simplecnn": SimpleCNN,
+    "unet": UnetTransfer,
+}
 
-# load config file
-with open(args.config, "r") as f:
-    config = yaml.safe_load(f)
-hyperparams = config["hyperparams"]
 
-# prepare data
-train_data, test_data = load_data()
-train_loader = DataLoader(train_data, batch_size=hyperparams["batch_size"])
-test_loader = DataLoader(test_data, batch_size=hyperparams["batch_size"])
+@dataclass
+class TrainingMetrics:
+    train_losses: list[float] = field(default_factory=list)
+    train_f1s: list[float] = field(default_factory=list)
+    test_losses: list[float] = field(default_factory=list)
+    test_f1s: list[float] = field(default_factory=list)
 
-if args.simplecnn:
-    model = SimpleCNN()
-elif args.unet:
-    model = UnetTransfer()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams["learning_rate"])
-criterion = torch.nn.CrossEntropyLoss()
+def save_model(model, optimizer, metrics, file_path):
+    torch.save(
+        {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "metrics": asdict(metrics)},
+        file_path,
+    )
 
-checkpoint_dir = os.path.join(args.output_dir, config["exp_name"])
-os.makedirs(checkpoint_dir, exist_ok=True)
-train(model, criterion, optimizer, train_loader, test_loader, checkpoint_dir, nepochs=hyperparams["nepochs"])
+
+def train(model, criterion, optimizer, train_loader, test_loader, output_dir, nepochs=60, device="cpu"):
+    model = model.to(device)
+
+    metrics = TrainingMetrics()
+    for epoch in range(nepochs):
+        model.train()
+        train_loss = 0
+        train_f1 = 0
+        for img, labels in train_loader:
+            img = img.to(device)
+            labels = labels.to(device)
+            pred = model(img)
+
+            loss = criterion(pred, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.cpu().detach() / len(train_loader)
+            train_f1 += f1_score(
+                labels.cpu().detach(), torch.argmax(pred.cpu().detach(), dim=-1), average="weighted"
+            ) / len(train_loader)
+
+        metrics.train_losses.append(train_loss)
+        metrics.train_f1s.append(train_f1)
+
+        model.eval()
+        test_loss = 0
+        test_f1 = 0
+        for img, labels in test_loader:
+            img = img.to(device)
+            labels = labels.to(device)
+            pred = model(img)
+
+            loss = criterion(pred, labels)
+
+            test_loss += loss.cpu().detach() / len(test_loader)
+            test_f1 += f1_score(
+                labels.cpu().detach(), torch.argmax(pred.cpu().detach(), dim=-1), average="weighted"
+            ) / len(test_loader)
+
+        metrics.test_losses.append(test_loss)
+        metrics.test_f1s.append(test_f1)
+
+        if epoch % 5 == 0:
+            save_model(model, optimizer, metrics, Path(output_dir) / f"chk_{epoch}.pth")
+            print(
+                f"{epoch}: Test loss {metrics.test_losses[-1]:.4f}\t f1: {metrics.test_f1s[-1]:.3f} \t "
+                f"Train loss: {metrics.train_losses[-1]:.3f}\t f1: {metrics.train_f1s[-1]:.3f}"
+            )
+
+    save_model(model, optimizer, metrics, Path(output_dir) / f"chk_{epoch}.pth")
+
+    return model, metrics
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", required=True)
+    parser.add_argument("-o", "--output-dir", default="cnn/checkpoints")
+    parser.add_argument("-d", "--device", default="cpu")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+    hyperparams = config["hyperparams"]
+
+    # save config file alongside with the model checkpoints
+    checkpoint_dir = Path(args.output_dir) / config["exp_name"]
+    checkpoint_dir.mkdir(parents=True, exist_ok=True)
+    with open(checkpoint_dir / Path(args.config).name, "w") as f:
+        yaml.dump(config, f)
+
+    train_data, test_data = load_data()
+    train_loader = DataLoader(train_data, batch_size=hyperparams["batch_size"])
+    test_loader = DataLoader(test_data, batch_size=hyperparams["batch_size"])
+
+    model = models[config["model"]]()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyperparams["learning_rate"])
+    criterion = torch.nn.CrossEntropyLoss()
+
+    train(
+        model,
+        criterion,
+        optimizer,
+        train_loader,
+        test_loader,
+        checkpoint_dir,
+        hyperparams["nepochs"],
+        config["device"],
+    )
